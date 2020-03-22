dabf8a432cd310b6f6132cbb8cda0266098e2606
Behemoth86 <megadeth86@mail.ru>
2020-03-22 11:08:18 +0400

    feat: add solution for morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	const arr = [];
+	expr = expr.split('');
+	for (let i = 0, j = 0; i < expr.length; i++) {
+			if (!arr[j]) {arr[j] = expr[i];}
+			else {arr[j] += expr[i];}
+			if (((i+1)%10===0) && (i!==0)) {j++;}
+	}
+	for (let i = 0; i<arr.length; i++) {
+			let letter = [];
+			for (let j = arr[i].length; j>1; j-=2) {
+				if (arr[i].slice(j-2, j)==='10') {letter.unshift('.');}
+				else if (arr[i].slice(j-2, j)==='11') {letter.unshift('-');}
+				else if (arr[i].slice(j-2, j)==='**') {letter.unshift('*');}
+			}
+			letter = letter.join('');
+			if (letter == '*****') {letter = ' ';}
+			else {letter = MORSE_TABLE[letter]}
+			arr[i] = letter;
+	}
+	return arr.join('');
 }
 
 module.exports = {
